28c9a5e8ab3209dead8c125506259fceb4fbbe74
hyew0nn <hyewon-0607@naver.com>
2026-04-22 23:39:45 +0900

    fix: @Repository 어노테이션 위치 수정

--- src/main/java/com/goggles/orderservice/infrastructure/repository/OrderJpaRepository.java
+++ src/main/java/com/goggles/orderservice/infrastructure/repository/OrderJpaRepository.java
@@ -3,7 +3,5 @@ package com.goggles.orderservice.infrastructure.repository;
 import com.goggles.orderservice.domain.entity.Order;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderJpaRepository extends JpaRepository<Order, UUID> {}
--- src/main/java/com/goggles/orderservice/infrastructure/repository/OrderRepositoryImpl.java
+++ src/main/java/com/goggles/orderservice/infrastructure/repository/OrderRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.goggles.orderservice.infrastructure.repository;
 
 import com.goggles.orderservice.domain.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RequiredArgsConstructor
 public class OrderRepositoryImpl implements OrderRepository {
   private final OrderJpaRepository orderJpaRepository;
