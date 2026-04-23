d7c90e3f580e7eb812a145aafb8495e05921038b
hyew0nn <hyewon-0607@naver.com>
2026-04-23 16:23:23 +0900

    feat: coupon VO에 couponCode 필드 추가

--- src/main/java/com/goggles/orderservice/domain/vo/Coupon.java
+++ src/main/java/com/goggles/orderservice/domain/vo/Coupon.java
@@ -18,25 +18,30 @@ public class Coupon {
   @Column(name = "coupon_id")
   private UUID couponId;
 
+  @Column(name = "coupon_code")
+  private String couponCode;
+
   @Column(name = "coupon_name", length = 100)
   private String couponName;
 
   @Column(name = "coupon_discount_rate", precision = 5, scale = 2)
   private BigDecimal couponDiscountRate;
 
-  public Coupon(UUID couponId, String couponName, BigDecimal couponDiscountRate) {
-    validate(couponId, couponName, couponDiscountRate);
+  public Coupon(UUID couponId, String couponCode, String couponName, BigDecimal couponDiscountRate) {
+    validate(couponId, couponCode, couponName, couponDiscountRate);
     this.couponId = couponId;
+    this.couponCode = couponCode;
     this.couponName = couponName;
     this.couponDiscountRate = couponDiscountRate;
   }
 
-  private static void validate(UUID couponId, String couponName, BigDecimal couponDiscountRate) {
+  private static void validate(UUID couponId, String couponCode, String couponName, BigDecimal couponDiscountRate) {
     boolean hasId = couponId != null;
+    boolean hasCode = couponCode != null && !couponCode.isBlank();
     boolean hasName = couponName != null && !couponName.isBlank();
 
-    if (hasId != hasName) {
-      throw new BadRequestException("couponId와 couponName은 함께 존재하거나 함께 없어야 합니다.");
+    if (hasId != hasCode || hasId != hasName) {
+      throw new BadRequestException("couponId, couponCode, couponName은 함께 존재하거나 함께 없어야 합니다.");
     }
 
     if (hasId && couponDiscountRate == null) {
