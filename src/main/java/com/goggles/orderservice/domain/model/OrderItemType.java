34f4b4fb007f871c612483f4a0275349bf8fbe30
hyew0nn <hyewon-0607@naver.com>
2026-04-26 17:21:37 +0900

    refactor: 주문 상품 타입 displayName 필드 추가

--- src/main/java/com/goggles/orderservice/domain/model/OrderItemType.java
+++ src/main/java/com/goggles/orderservice/domain/model/OrderItemType.java
@@ -1,6 +1,15 @@
 package com.goggles.orderservice.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderItemType {
-  COURSE,
-  MENTORING
+  COURSE("강의"),
+  MENTORING("멘토링");
+
+  private final String displayName;
+
+  OrderItemType(String displayName) {
+    this.displayName = displayName;
+  }
 }
