3d5f3f3fbac3613ce13ce7f1e37a01ed5154be40
hyew0nn <hyewon-0607@naver.com>
2026-05-05 20:15:32 +0900

    refactor: 주문 생성 응답 객체 필드 수정

--- src/main/java/com/goggles/orderservice/application/dto/result/CreateOrderResult.java
+++ src/main/java/com/goggles/orderservice/application/dto/result/CreateOrderResult.java
@@ -3,8 +3,15 @@ package com.goggles.orderservice.application.dto.result;
 import com.goggles.orderservice.domain.model.Order;
 import java.util.UUID;
 
-public record CreateOrderResult(UUID orderId, UUID studentId) {
+public record CreateOrderResult(UUID orderId, String productName, Long orderPrice) {
   public static CreateOrderResult from(Order order) {
-    return new CreateOrderResult(order.getId(), order.getOrderer().getStudentId());
+    return new CreateOrderResult(
+        order.getId(),
+        order.getItems().getFirst().getProduct().getProductName(),
+        order.getPrice().getFinalPrice());
+  }
+
+  public static CreateOrderResult of(Order order, String productName) {
+    return new CreateOrderResult(order.getId(), productName, order.getPrice().getFinalPrice());
   }
 }
--- src/main/java/com/goggles/orderservice/presentation/dto/CreateOrderResponse.java
+++ src/main/java/com/goggles/orderservice/presentation/dto/CreateOrderResponse.java
@@ -3,8 +3,8 @@ package com.goggles.orderservice.presentation.dto;
 import com.goggles.orderservice.application.dto.result.CreateOrderResult;
 import java.util.UUID;
 
-public record CreateOrderResponse(UUID orderId, UUID studentId) {
+public record CreateOrderResponse(UUID orderId, Long orderPrice, String productName) {
   public static CreateOrderResponse from(CreateOrderResult result) {
-    return new CreateOrderResponse(result.orderId(), result.studentId());
+    return new CreateOrderResponse(result.orderId(), result.orderPrice(), result.productName());
   }
 }
